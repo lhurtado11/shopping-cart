4fcadf48a64c1476d92da2993954050de8cdf8fc
Luisa <lhurtado372@gmail.com>
2021-05-13 13:07:11 -0500

    consume la api, pinta las cards en el html

---
+++ app.js
@@ -0,0 +1,33 @@
+// obteniendo elementos por su id
+const cards = document.getElementById('cards')
+// obteniendo templates
+const templateCard = document.getElementById('template-card').content
+
+const fragment = document.createDocumentFragment()
+// capturar los datos
+document.addEventListener('DOMContentLoaded', () => {
+    fetchData()
+}) 
+// consumiendo la api.json
+const fetchData = async () => {
+    try {
+        const res = await fetch('./assets/api.json') 
+        const data = await res.json()
+        pintarCards(data)
+    } 
+    catch (error) {
+        console.log(error);
+    }
+}
+// pintar catalogo
+const pintarCards = data => {
+    data.forEach(producto => {
+        templateCard.querySelector('h5').textContent = producto.title
+        templateCard.querySelector('p').textContent = producto.precio
+        templateCard.querySelector('img').setAttribute('src', producto.thumbnailUrl)
+        templateCard.querySelector('.btn-dark').dataset.id = producto.id
+        const clone = templateCard.cloneNode(true)
+        fragment.appendChild(clone)
+ })
+ cards.appendChild(fragment)
+}
